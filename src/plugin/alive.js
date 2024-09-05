968f08ea709effec82e412213c3743ee7afafe60
binuka2009 <binukaudayanga3@gmail.com>
2024-09-05 21:34:28 +0530

    Create alive.js

---
+++ src/plugin/alive.js
@@ -0,0 +1,88 @@
+import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg;
+import config from '../../config.cjs';
+
+const alive = async (m, Matrix) => {
+  const uptimeSeconds = process.uptime();
+  const days = Math.floor(uptimeSeconds / (24 * 3600));
+  const hours = Math.floor((uptimeSeconds % (24 * 3600)) / 3600);
+  const minutes = Math.floor((uptimeSeconds % 3600) / 60);
+  const seconds = Math.floor(uptimeSeconds % 60);
+  
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+    if (['alive', 'uptime', 'runtime'].includes(cmd)) {
+
+  const uptimeMessage = `*➤ BINUKA-MD Status Overview*
+_________________________________________
+
+*📆 ${days} Day*
+*🕰️ ${hours} Hour*
+*⏳ ${minutes} Minute*
+*⏲️ ${seconds} Second*
+_________________________________________
+`;
+
+  const buttons = [
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "MENU",
+            id: `${prefix}menu`
+          })
+        },
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "PING",
+            id: `${prefix}ping`
+          })
+        }
+        ];
+
+  const msg = generateWAMessageFromContent(m.from, {
+    viewOnceMessage: {
+      message: {
+        messageContextInfo: {
+          deviceListMetadata: {},
+          deviceListMetadataVersion: 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: uptimeMessage
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙɪɴᴜᴋᴀ ᴍᴅ"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "",
+            gifPlayback: true,
+            subtitle: "",
+            hasMediaAttachment: false 
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons
+          }),
+          contextInfo: {
+                  quotedMessage: m.message,
+                  mentionedJid: [m.sender], 
+                  forwardingScore: 999,
+                  isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                  newsletterJid: '120363249960769123@newsletter',
+                  newsletterName: "Binuka-MD-V1",
+                  serverMessageId: 143
+                }
+              }
+        }),
+      },
+    },
+  }, {});
+
+  await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
+    messageId: msg.key.id
+  });
+    }
+};
+
+export default alive;
